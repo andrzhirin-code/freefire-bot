ef88c442fca0e9358fcf2ca6d40ac12ec3a0fd37
andrzhirin-code <andr.zhirin@gmail.com>
2026-04-26 21:39:57 +0500

    states.py

--- states.py
+++ states.py
@@ -1,14 +1,11 @@
-# states.py
-# Состояния пользователей (FSM - конечный автомат)
-
-MENU = "MENU"                 # Главное меню
-FREE_PHONES = "FREE_PHONES"   # Выбор бесплатной модели
-AI_WAITING = "AI_WAITING"     # Ожидание оплаты
-AI_ASK_PHONE = "AI_ASK_PHONE"      # Вопрос 1: телефон
-AI_ASK_RAM = "AI_ASK_RAM"          # Вопрос 2: ОЗУ
-AI_ASK_STYLE = "AI_ASK_STYLE"      # Вопрос 3: стиль игры
-AI_ASK_WEAPON = "AI_ASK_WEAPON"    # Вопрос 4: оружие
-AI_ASK_FINGERS = "AI_ASK_FINGERS"  # Вопрос 5: пальцы
-AI_ASK_PROBLEM = "AI_ASK_PROBLEM"  # Вопрос 6: проблема
-AI_DONE = "AI_DONE"           # Настройка выдана, ожидание корректировки
-CORRECTION = "CORRECTION"     # Режим корректировки
+MENU = "MENU"
+FREE_PHONES = "FREE_PHONES"
+AI_WAITING = "AI_WAITING"
+AI_ASK_PHONE = "AI_ASK_PHONE"
+AI_ASK_RAM = "AI_ASK_RAM"
+AI_ASK_STYLE = "AI_ASK_STYLE"
+AI_ASK_WEAPON = "AI_ASK_WEAPON"
+AI_ASK_FINGERS = "AI_ASK_FINGERS"
+AI_ASK_PROBLEM = "AI_ASK_PROBLEM"
+AI_DONE = "AI_DONE"
+CORRECTION = "CORRECTION"
